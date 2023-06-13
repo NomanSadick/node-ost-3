d561148d8a098a8497caf9fe9e79f89efe90772c
Noman Sadick <nomansadickjs@gmail.com>
2023-06-13 18:52:48 +0600

    fs module

--- index.js
+++ index.js
@@ -1,18 +1,76 @@
 const http=require('http');
-const URL=require('url');
+// const URL=require('url');
+const fs = require('fs');
 
 const server=http.createServer(function (req,res) {
-    const myURL="http://rabbil.com/blog.html?year=2020&month=july";
-    const myURLObj= URL.parse(myURL,true);
+    // asy 
+    // if (req.url == '/') {
+    //     const myData = fs.readFileSync('index.html')
+    //     res.writeHead(200, {'Content-Type': 'text'})
+    //     res.write(myData);
+    //     res.end();
+    // }
 
-    const myHostName=myURLObj.host;
-    const myPathName=myURLObj.pathname;
-    const mySearchName=myURLObj.search;
+    // asy write
+    // if (req.url == '/') {
+    //     fs.writeFile('demo.txt','Hello World!', function (err) {
+    //         if (err) {
+    //             res.writeHead(200, {'Content-Type':'text'})
+    //             res.write("File Write fail");
+    //             res.end();
+    //         } else {
+    //             res.writeHead(200, {'Content-Type': 'text'})
+    //             res.write("File Write Successfully!");
+    //             res.end();
+    //         }
+    //     });
+    // }
 
-    res.writeHead(200,{'Content-Type':'text/html'})
-    res.write(myHostName);
-    res.end();
+    // sync write 
+    // if (req.url == '/') {
+    //     let err = fs.writeFileSync('demoSync.txt','Hello World!');
+    //         if (err) {
+    //             res.writeHead(200, {'Content-Type':'text'})
+    //             res.write("File Write fail");
+    //             res.end();
+    //         } else {
+    //             res.writeHead(200, {'Content-Type': 'text'})
+    //             res.write("File Write Successfully!");
+    //             res.end();
+    //         }
+    // }
+
+    // asy rename 
+    // if (req.url == '/') {
+    //     fs.rename('demo.txt', 'demoNew.txt', function (err){
+    //         if (err) {
+    //             res.writeHead(200, {'Content-Type':'text'})
+    //             res.write("File Rename fail");
+    //             res.end();
+    //             } else {
+    //                 res.writeHead(200, {'Content-Type': 'text'})
+    //                 res.write("File Rename Successfully!");
+    //                 res.end();
+    //         }
+    //     })
+    // }
+
+    // asy delete 
+    if (req.url == '/') {
+        fs.unlink('demoNew.txt',function (err){
+            if (err) {
+                res.writeHead(200, {'Content-Type':'text'})
+                res.write("File Rename fail");
+                res.end();
+            }else {
+                res.writeHead(200, {'Content-Type': 'text'})
+                res.write("File Rename Successfully!");
+                res.end();
+            }
+        })
+    }
 });
 
-server.listen(5050);
+
+server.listen(3000);
 console.log("Server Run Success");
